9fa22bf92ca31e806fce73a5fbf06fd240cbdb44
Skirmantas Jurgaitis <skyjur@gmail.com>
2018-02-04 13:10:52 +0800

    Remove sensitive config information

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,26 @@
+import {env} from 'process';
+
+require('dotenv').config();
+
 export const fb = {
-    accessToken: 'EAACgZCYOrzZAEBALOVFE0fjCtpSqlWoOxMwPL9lobi9ZAcOHu1zO9FD02pzdktkMa3s7xYtNdx0xmtMJtpF1W5KkHekfrfHCZCBnioIrsJ1DyjLkaLe4XDBAvKzZB6vnOLJeClvNpqvz85ZCaW9W0MW8ReyKtIZA8j5TpgMemVeQwZDZD',
-    verifyToken: '123456',
-    appSecret: 'f0df79a7a979328632033a0b61de7173'
+    accessToken: env.FB_ACCESS_TOKEN,
+    verifyToken: env.FB_VERIFY_TOKEN,
+    appSecret: env.FB_APP_SECRET
 }
 
 export const dialogFlow = {
-    accessToken: 'd2177851ee2e4bd199c1a4d5fd07b8d6'
+    accessToken: env.DIALOGLOW_ACCESS_TOKEN
 }
 
 export const googleMaps = {
-    apiKey: 'AIzaSyCYnUO6wBYVHA0LfaKLJSUhUJHPLDq7m2Q'
+    apiKey: env.GOOGLE_MAPS_API_KEY
 }
+
+export const webApp = {
+    port: parseInt( env.PORT || '8000' )
+}
+
+console.log('setttings:fb', JSON.stringify(fb));
+console.log('setttings:dialogFlow', JSON.stringify(dialogFlow));
+console.log('setttings:googleMaps', JSON.stringify(googleMaps));
+console.log('setttings:webApp', JSON.stringify(webApp));
